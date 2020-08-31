0e34be07862a6c55125ccda90a1463ffee2eb5d2
Minjeong <mathmjseo@naver.com>
2020-08-31 18:34:21 +0900

    ubuntu os 용 업데이트 및 대시보드 수정 dangerzone 실행은 됨, 하지만 파일을 사용자에게 넘겨주는 것은 아직 안됨.

--- webpageapp/views.py
+++ webpageapp/views.py
@@ -9,35 +9,59 @@ from .forms import UploadDocumentForm
 from django.core.files.storage import FileSystemStorage
 import datetime
 from django import get_version
-from django.http import FileResponse
+from django.http import FileResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
-g_number_of_visitor = 0
-g_number_of_file = 0
-is_same = False
+
+g_number_of_visitor = {'8-30':30}
+g_number_of_file = {'8-30':5}
+date = str(datetime.date.today().month) + '-' + str(datetime.date.today().day)
 # Create your views here.
+
 def index(request):
-    print(get_version())
-    global g_number_of_visitor, g_number_of_file
-    g_number_of_visitor += 1
+    global g_number_of_visitor, g_number_of_file,date
+    if g_number_of_visitor.get(date): #해당 날짜의 기록이 존재하면
+        g_number_of_visitor[date] += 1
+    else:
+        g_number_of_visitor[date] = 1
+
+    if not g_number_of_file.get(date):
+        tmp = list(g_number_of_file.values())
+        g_number_of_file[date] = tmp[len(tmp)-1]
 
     folder = 'media/my_folder/'
     if request.method=='POST' and request.FILES['inputFile']:
-        g_number_of_file += 1
+        if g_number_of_file.get(date):  # 해당 날짜의 기록이 존재하면
+            g_number_of_file[date] += 1
+        else:
+            if len(g_number_of_file) == 0:
+                g_number_of_file[date] = 1
+            else:
+                temp = list(g_number_of_file.values())
+                g_number_of_file[date] = temp[len(temp) - 1] + 1  # 누적
+
         myfile = request.FILES['inputFile']
+
         fs = FileSystemStorage(location=folder)
         fs.save(myfile.name, myfile)
         filename = myfile.name
-        print(filename)
-        # os.remove(media\my_folder\+filename, option)
+        print("파일명", filename)
         file_url = fs.url(filename)
-        # uploadpath = " " + path + filename + " "
-        # subprocess.call(["/usr/bin/dangerzone-container" " documenttopixels --document-filename" + uploadpath + "--pixel-dir /tmp/dangerzone-pixel --container-name flmcode/dangerzone"],shell=True)
-        # subprocess.call(["/usr/bin/dangerzone-container" " pixelstopdf --pixel-dir /tmp/dangerzone-pixel --safe-dir /tmp/dangerzone-safe --container-name flmcode/dangerzone --ocr 0 --ocr-lang eng"],shell=True)
+        path = '/home/ubuntu/hanium-dangerzone-opensource/media/my_folder/'
+        uploadpath = " " + path + filename + " "
+        subprocess.call(["/usr/bin/dangerzone-container" " documenttopixels --document-filename" + uploadpath + "--pixel-dir /tmp/dangerzone-pixel --container-name flmcode/dangerzone"],shell=True)
+        subprocess.call(["/usr/bin/dangerzone-container" " pixelstopdf --pixel-dir /tmp/dangerzone-pixel --safe-dir /tmp/dangerzone-safe --container-name flmcode/dangerzone --ocr 0 --ocr-lang eng"],shell=True)
         # os.rename("/tmp/dangerzone-safe/safe-output-compressed.pdf",
         #           "/tmp/dangerzone-safe/" + filename + "_" + "safe-output.pdf")
-        return render(request, 'fileupload.html',{'file_url':file_url})
+        file_url = '/tmp/dangerzone-safe/safe-output-compressed.pdf'
+        rm_file = 'media/my_folder/'+filename
+        if os.path.isfile(rm_file):
+            os.remove(rm_file)
+            print(rm_file,"is deleted")
+        return render(request, 'fileupload.html')
     else:
         return render(request, 'index.html')
+
     #
     # form = UploadDocumentForm()
     # if request.method == 'POST':
@@ -52,15 +76,28 @@ def index(request):
     #     #     return HttpResponseRedirect('/fileupload')
     # return render(request, 'index.html',{'form':form})
 
+@csrf_exempt
 def contact(request):
     return render(request, 'contact.html')
 
 def dashboard(request):
-    return render(request, 'dashboard.html',{'cnt':g_number_of_visitor, 'file_cnt': g_number_of_file})
+    total_num_of_visitor_label = list(g_number_of_visitor.keys())
+    total_num_of_file_label = list(g_number_of_file.keys())
+    total_num_of_visitor = list(g_number_of_visitor.values())
+    total_num_of_file = list(g_number_of_file.values())
+
+    return render(request, 'dashboard.html',{'cnt':g_number_of_visitor[date],
+                                             'file_cnt': g_number_of_file[date],
+                                             'visitor_data':total_num_of_visitor,
+                                             'file_data':total_num_of_file,
+                                             'visitor_label': total_num_of_visitor_label,
+                                             'file_label': total_num_of_file_label
+                                             } )
 
 def fileupload(request):
     return render(request,'fileupload.html')
 
+@csrf_exempt
 def sendmail(request):
     return render(request,'sendmail.html')
 
