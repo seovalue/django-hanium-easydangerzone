8089a7e975eb3300d9f86a5cddcaddd8fbe2d98d
Minjeong <mathmjseo@naver.com>
2020-09-02 00:33:32 +0900

    Fix bugs and update README README의 update0902에 나열된 버그들을 fix함.

--- webpageapp/models.py
+++ webpageapp/models.py
@@ -2,8 +2,12 @@
 from django.db import models
 
 # Create your models here
-def get_file_path(instance, filename):
-    return '/upload_file'
+# def get_file_path(instance, filename):
+#     return '/upload_file'
+#
+# upload_files = models.FileField(upload_to=get_file_path, null=True, blank=True, verbose_name='파일')
+# filename = models.CharField(max_length=64, null=True, verbose_name='첨부파일명')
 
-upload_files = models.FileField(upload_to=get_file_path, null=True, blank=True, verbose_name='파일')
-filename = models.CharField(max_length=64, null=True, verbose_name='첨부파일명')
+class Visitor(models.Model):
+    date = models.DateField()
+    number = models.IntegerField()
